9f2a7938248ced8c0da6ce06d81914f271bb1906
Matt <matthaerle@gmail.com>
2017-04-13 14:58:43 -0500

    getJSONStringwithPostData

---
+++ app/src/main/java/com/acusportrtg/axismobile/Methods/GetJSONStringWithPOSTData.java
@@ -0,0 +1,14 @@
+package com.acusportrtg.axismobile.Methods;
+
+import java.net.URL;
+
+/**
+ * Created by Matt on 4/12/2017.
+ */
+
+public class GetJSONStringWithPOSTData {
+    public String GetJsonString (URL url) {
+
+        return null;
+    }
+}
