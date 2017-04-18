f1ca95943e1792dfe97b0d2e2bed7bfd973a1859
mhaerle <mhaerle@acusport.com>
2017-04-18 16:30:42 -0500

    Working on drawer

--- app/src/main/java/com/acusportrtg/axismobile/Drawer_Activity.java
+++ app/src/main/java/com/acusportrtg/axismobile/Drawer_Activity.java
@@ -13,16 +13,24 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class Navigation_Drawer_Activity extends AppCompatActivity
+public class Drawer_Activity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_navigation_drawer);
+        setContentView(R.layout.drawer_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -47,7 +55,7 @@ public class Navigation_Drawer_Activity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.navigation__drawer_, menu);
+        getMenuInflater().inflate(R.menu.drawer_, menu);
         return true;
     }
 
--- app/src/main/java/com/acusportrtg/axismobile/Inventory_Type_Select_Activity.java
+++ app/src/main/java/com/acusportrtg/axismobile/Inventory_Type_Select_Activity.java
@@ -1,11 +1,20 @@
 package com.acusportrtg.axismobile;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import com.acusportrtg.axismobile.Methods.MyDrawerLayout;
 
 import com.acusportrtg.axismobile.Methods.Globals;
 
@@ -17,11 +26,20 @@ import org.w3c.dom.Text;
 public class Inventory_Type_Select_Activity extends AppCompatActivity {
     //private TextView txt_Emp;
     private Button btn_Non_Firearm_Inventory, btn_Firearm_Inventory;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private String myActivityTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inventory_task_chooser);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        myActivityTitle = getSupportActionBar().getTitle().toString();
+
+
         /*txt_Emp = (TextView)findViewById(R.id.txtEmpId);
 
         Globals glob = ((Globals)getApplicationContext());
@@ -40,4 +58,34 @@ public class Inventory_Type_Select_Activity extends AppCompatActivity {
 
     }
 
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.drawer_open,R.string.drawer_close) {
+            public void OnDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("Navigation!");
+                invalidateOptionsMenu();
+            }
+            public void OnDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(myActivityTitle);
+                invalidateOptionsMenu();
+            }
+        };
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.navigation_drawer_, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
