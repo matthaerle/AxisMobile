8bc6db99b1d769e849526561a38cdfe1c7c5d065
jpederson <itsmejasonp@gmail.com>
2017-04-21 19:51:54 -0500

    Firearm Scan Activity

--- app/src/main/java/com/acusportrtg/axismobile/Firearm_Scan_Activity.java
+++ app/src/main/java/com/acusportrtg/axismobile/Firearm_Scan_Activity.java
@@ -2,6 +2,8 @@ package com.acusportrtg.axismobile;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RadioButton;
 
 /**
  * Created by mhaerle on 4/21/2017.
@@ -11,5 +13,29 @@ public class Firearm_Scan_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.firearm_inventory_scan);
+
+        final RadioButton radio_serial = (RadioButton) findViewById(R.id.rdl_serial_number);
+        final RadioButton radio_log = (RadioButton) findViewById(R.id.rdl_log_number);
+
+
+
+        View.OnClickListener radio_serial_listener = new View.OnClickListener(){
+            public void onClick(View v) {
+                if(radio_log.isChecked()){
+                    radio_log.setChecked(false);
+                }
+            }
+        };
+        View.OnClickListener radio_log_listener = new View.OnClickListener(){
+            public void onClick(View v) {
+                if(radio_serial.isChecked()){
+                    radio_serial.setChecked(false);
+                }
+            }
+        };
+
+        radio_serial.setOnClickListener(radio_serial_listener);
+        radio_log.setOnClickListener(radio_log_listener);
+
     }
 }
