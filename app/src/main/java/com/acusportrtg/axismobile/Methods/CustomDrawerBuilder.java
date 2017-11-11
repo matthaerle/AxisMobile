9780156f8cf88525a90df0b701edcc4d3be78499
jpederson <itsmejasonp@gmail.com>
2017-11-10 20:07:07 -0600

    Updates to: EmployeeRoles Drawer

--- app/src/main/java/com/acusportrtg/axismobile/Methods/CustomDrawerBuilder.java
+++ app/src/main/java/com/acusportrtg/axismobile/Methods/CustomDrawerBuilder.java
@@ -3,16 +3,23 @@ package com.acusportrtg.axismobile.Methods;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.acusportrtg.axismobile.AppSettingsActivity;
 import com.acusportrtg.axismobile.Globals;
 import com.acusportrtg.axismobile.InventoryFirearmsActivity;
 import com.acusportrtg.axismobile.InventoryProductsActivity;
 import com.acusportrtg.axismobile.Inventory_Task;
+import com.acusportrtg.axismobile.JSON_Classes.EmployeeRoles;
 import com.acusportrtg.axismobile.JSON_Classes.GetEmployees;
+import com.acusportrtg.axismobile.JSON_Classes.SendProductView;
 import com.acusportrtg.axismobile.LoginActivity;
 import com.acusportrtg.axismobile.R;
 import com.acusportrtg.axismobile.SearchFirearmsActivity;
@@ -30,6 +37,16 @@ import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+import static android.content.ContentValues.TAG;
+
 /**
  * Created by mhaerle on 10/31/2017.
  */
@@ -37,9 +54,38 @@ import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 public class CustomDrawerBuilder {
     private IProfile profile;
     private AccountHeader headerResult = null;
+    private String JSONReturnData = "";
+    private EmployeeRoles empRoles = new EmployeeRoles();
     public Drawer CustomDrawer(Context context, Activity activity, Toolbar toolbar, Drawer result, Bundle savedInstanceState, GetEmployees employee) {
         final Context context1 = context;
         final Globals globals = new Globals();
+
+        JSONObject postData = new JSONObject();
+        try {
+            URL reqUrl = new URL("http://" + SharedPrefs.GetSavedServerAddress(context) + ":8899/RestWCFServiceLibrary/GetEmployeeRoles");
+            postData.put("EmployeeID",employee.getEmployeeID());
+            GetJSONStringWithPOSTData.GetJSONDataBack getJSONDataBack = new GetJSONStringWithPOSTData.GetJSONDataBack(context) {
+                @Override
+                public void receiveData(Object object) {
+                    JSONReturnData = (String)object;
+                    try {
+                        JSONArray productJson = new JSONArray(JSONReturnData);
+                        empRoles = GetEmployeeRoles(productJson);
+
+                    } catch (final JSONException e) {
+                        Log.e(TAG, "JSON parsing error: " + e.getMessage());
+                    }
+
+                }};
+            getJSONDataBack.execute(reqUrl.toString(), postData.toString());
+            Log.v(TAG,JSONReturnData);
+
+        } catch (MalformedURLException e) {
+            Log.e(TAG, "MalformedURLException: " + e.getMessage() + "\n" + e.getLocalizedMessage());
+        } catch (Exception e) {
+            Log.e(TAG, "Exception: " + e.getMessage() + "\n" + e.getLocalizedMessage());
+        }
+
 
         profile = new ProfileDrawerItem().withName(employee.getFirstName() + ' ' + employee.getLastName());
         buildHeader(false, savedInstanceState, activity);
@@ -87,7 +133,44 @@ public class CustomDrawerBuilder {
                 )
                 .withSavedInstance(savedInstanceState)
                 .build();
+        if(!empRoles.getInventoryManagementPermission()){
+            result.removeItem(1);
+        }
+        if(!empRoles.getFirearmsPermission()){
+            Log.v(TAG,empRoles.getFirearmsPermission().toString());
+            result.removeItem(2);
+        }
+        if(!empRoles.getFirearmPhysicalInventory() && !empRoles.getIMProdMaintAdjQoHPermission()){
+            result.removeItem(3);
+        }
+
         return result;
+    }
+
+    private EmployeeRoles GetEmployeeRoles(JSONArray employeeRolesJson){
+        EmployeeRoles empR = new EmployeeRoles();
+        try{
+            //JSONArray productJson = new JSONArray(jsonStr);
+            if(employeeRolesJson.length() == 1){
+                for (int i=0; i <employeeRolesJson.length(); i++) {
+                    JSONObject p = employeeRolesJson.getJSONObject(i);
+
+                    empR.setFirearmBoundBookPermission(p.has("FirearmBoundBook"));
+                    empR.setFirearmsPermission(p.has("Firearms"));
+                    empR.setFirearmPhysicalInventoryPermission(p.has("FirearmPhysicalInventory"));
+                    empR.setFirearmViewCostPermission(p.has("FirearmViewCost"));
+                    empR.setPricingPermission(p.has("Pricing"));
+                    empR.setProductCostPermission(p.has("ProductCost"));
+                    empR.setIMProdMaintAdjQoHPermission(p.has("IMProdMaintAdjQoH"));
+                    empR.setIMUpdatePermission(p.has("IMUpdate"));
+                    empR.setInventoryManagementPermission(p.has("InventoryManagement"));
+                }
+            }
+
+        } catch (final JSONException e) {
+            Log.e(TAG, "JSON parsing error: " + e.getMessage());
+        }
+        return empR;
     }
 
     private void buildHeader(boolean b, Bundle savedInstanceState,Activity activity) {
